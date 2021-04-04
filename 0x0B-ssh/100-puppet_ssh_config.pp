239dbd75d122465c5f3b4c3a78e2a72157c08c4d
aristizabaru <andres.aristizabal@gmail.com>
2021-04-04 21:03:09 +0000

    Refactor: change name of resourse

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Configure config global file
-file_line { 'Set IdentityFile':
+file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
-file_line { 'Set PasswordAuthentication':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
