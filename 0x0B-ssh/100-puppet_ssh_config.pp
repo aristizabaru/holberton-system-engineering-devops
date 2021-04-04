3347a96d78c51b8025d0926c336a8af5da52d020
aristizabaru <andres.aristizabal@gmail.com>
2021-04-04 20:45:51 +0000

    Fix: present command within commas

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,12 @@
 include stdlib
 
 file_line { 'Set IdentityFile':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
 file_line { 'Set PasswordAuthentication':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
-
