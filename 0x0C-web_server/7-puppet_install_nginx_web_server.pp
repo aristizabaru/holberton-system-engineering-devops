491390f211a96ae121079c3d50328e78fa2408d2
aristizabaru <andres.aristizabal@gmail.com>
2021-04-08 03:32:48 +0000

    Fix: add enviroment variable in redirection

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -19,13 +19,13 @@ exec { 'Change_index':
   returns => [0,1]
 }
 
-exec { 'Redirection':
-  require     => Exec['Change_index'],
-  environment => ['command="\\\n\t# Redirection\n\trewrite ^/redirect_me/(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"'],
-  command     => 'sudo sed -i "/server_name _;/ a $command" /etc/nginx/sites-available/default',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0,1]
-}
+exec { 'Redirection':                                                                                                                          
+  require     => Exec['Change_index'],                                                                                                         
+  environment => ["command='\\\n\t# Redirection\n\trewrite ^/redirect_me/(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"],                                                                                       
+  command     => "sed -i '/server_name _;/ a $command' /etc/nginx/sites-availablle/default",                                                                                                                                    
+  path        => ['/usr/bin', '/bin'],                                                                                                         
+  returns     => [0,1]                                                                                                                         
+}   
 
 exec { 'Start_service':
   require => Exec['Redirection'],
