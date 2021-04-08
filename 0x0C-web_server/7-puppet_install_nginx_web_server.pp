1c36178ea23ec547ce7adb8913056c67c2b29d10
aristizabaru <andres.aristizabal@gmail.com>
2021-04-08 02:45:08 +0000

    Fix: add expected return to resource

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,12 +2,14 @@
 exec { 'Update packages DB':
   command => 'apt-get update -y',
   path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
 exec { 'Install Nginx':
   require => Exec['Update packages DB'],
   command => 'apt-get install nginx -y',
   path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
 exec { 'Change index.html':
@@ -22,10 +24,12 @@ exec { 'Redirection':
   environment => ['command="\\\n\t# Redirection\n\trewrite ^/redirect_me/(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"'],
   command     => 'sed -i "/server_name _;/ a $command" /etc/nginx/sites-available/default',
   path        => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
 exec { 'Start service':
   require => Exec['Redirection'],
   command => 'sudo service nginx start',
-  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
