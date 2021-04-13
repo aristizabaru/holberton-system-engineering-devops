18ad9e06395e7860606e85b9d5f18e19870acc52
aristizabaru <andres.aristizabal@gmail.com>
2021-04-13 00:51:16 +0000

    Fix: change commands

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,23 @@
 # Configures Nginx web server
 exec { 'Update_packages_DB':
-  command => 'sudo apt-get update -y',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+  command  => 'sudo apt-get update',
+  provider => 'shell',
+  user     => 'root',
 }
 
-exec { 'Install_Nginx':
-  require => Exec['Update_packages_DB'],
-  command => 'sudo apt-get install nginx -y',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+package { 'nginx':
+  ensure   => installed,
 }
 
-exec { 'Change_index':
-  require => Exec['Install_Nginx'],
-  command => 'sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+file_line { 'add_header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By $hostname;'
 }
 
-exec { 'Header':                                                                                                                          
-  require     => Exec['Change_index'],                                                                                                                                                                                               
-  command     => "sed -i '/rewrite/ a \n\t# Add header directive\n\tadd_header X-Served-By \$hostname;\n' /etc/nginx/sites-available/default",                                                                                                                                    
-  path        => ['/usr/bin', '/bin'],                                                                                                         
-  returns     => [0,1]                                                                                                                         
-}   
-
-exec { 'Start_service':
-  require => Exec['Header'],
-  command => 'sudo service nginx start',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx']
 }
