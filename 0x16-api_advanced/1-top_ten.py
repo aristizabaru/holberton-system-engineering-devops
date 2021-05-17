b9edf52782d02dea5e817bcc0cee9c5c16c33559
aristizabaru <andres.aristizabal@gmail.com>
2021-05-17 19:11:40 +0000

    Fix: remove query limit from END_POINT string

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,7 +16,7 @@ def top_ten(subreddit):
         HEADERS = {'User-Agent': 'Python:Holberton School '
                    'API project:v1.0.0 (by /u/aristizabaru)'}
         END_POINT = 'https://www.reddit.com/r/{}/hot.json'\
-                    .format(subreddit, LIMIT)
+                    .format(subreddit)
 
         response = requests.get(
             END_POINT,
@@ -26,7 +26,7 @@ def top_ten(subreddit):
         if response.status_code == requests.codes.ok:
             nodes = response.json()['data']['children']
 
-            if len(nodes) is not None and nodes[0].get('kind') == 't3':
+            if len(nodes) is not None and nodes[0]['kind'] == 't3':
                 for item in nodes:
                     print(item['data']['title'])
             else:
